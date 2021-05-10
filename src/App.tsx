865dbba1c8b89023aaeb66726567a7d6f9b484e4
Pasanmahee <pasanmahee.roo@gmail.com>
2021-05-11 05:26:21 +0530

    tailwindcss configed

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,18 @@
 import React, { Component }  from 'react';
+import StyledForm from './styles/StyledForm';
 import './App.less';
 
 class App extends React.Component {
   render() {
     return (
-      <div>Hello world</div>
+      <StyledForm>
+      <form>
+        <input type="text" placeholder="Full name" />
+        <input type="text" placeholder="Email" />
+        <input type="text" placeholder="Password" />
+        <button>Sign In</button>
+      </form>
+    </StyledForm>
     )
   }
 }
