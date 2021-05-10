18b480cf8acf3e11ae08daa127ce88bd97ed7d58
Pasanmahee <pasanmahee.roo@gmail.com>
2021-05-11 03:51:07 +0530

    app initiated with typescript

---
+++ config-overrides.js
@@ -0,0 +1,6 @@
+const rewireLess = require('react-app-rewire-less');
+
+module.exports = function override(config, env) {
+    //Override config here. 
+    return config;
+};
---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import React, { Component }  from 'react';
+import './App.less';
+
+class App extends React.Component {
+  render() {
+    return (
+      <div>Hello world</div>
+    )
+  }
+}
+
+export default App;
